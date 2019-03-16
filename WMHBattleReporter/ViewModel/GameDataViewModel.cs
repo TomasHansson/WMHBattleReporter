60cef97e53b1cb457ab62c702ed790c320defdf3
Tomas Hansson <45672109+TomasHansson@users.noreply.github.com>
2019-03-16 11:04:33 +0100

    Add GameDataUserControl that shows a list of all BattleReports.

---
+++ WMHBattleReporter/ViewModel/GameDataViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMHBattleReporter.Model;
+
+namespace WMHBattleReporter.ViewModel
+{
+    public class GameDataViewModel
+    {
+        public ObservableCollection<BattleReport> BattleReports { get; set; } = new ObservableCollection<BattleReport>();
+
+        public GameDataViewModel()
+        {
+            FillCollection();
+        }
+
+        private void FillCollection()
+        {
+            List<BattleReport> battleReports = DatabaseServices.GetBattleReports().OrderByDescending(br => br.DatePlayed).ToList();
+            foreach (BattleReport battleReport in battleReports)
+                BattleReports.Add(battleReport);
+        }
+    }
+}

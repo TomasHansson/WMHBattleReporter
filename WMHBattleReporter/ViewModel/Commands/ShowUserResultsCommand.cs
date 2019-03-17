b30c9a3c2bb42a7a95ef695303a37b48e4e65344
Tomas Hansson <45672109+TomasHansson@users.noreply.github.com>
2019-03-17 15:37:44 +0100

    Refactor FillTopUsersCollection to use several helper-functions with the intention of making it more readable.

--- WMHBattleReporter/ViewModel/Commands/ShowUserResultsCommand.cs
+++ WMHBattleReporter/ViewModel/Commands/ShowUserResultsCommand.cs
@@ -40,7 +40,6 @@ namespace WMHBattleReporter.ViewModel.Commands
         private void FillTopUsersCollection()
         {
             ViewModel.TopUsersResult.Clear();
-
             List<User> topTenUsers = DatabaseServices.GetUsers().OrderByDescending(u => u.Winrate).Take(10).ToList();
 
             foreach (User user in topTenUsers)
@@ -53,98 +52,24 @@ namespace WMHBattleReporter.ViewModel.Commands
                 Dictionary<string, int> gamesWonWithEachTheme = new Dictionary<string, int>();
                 Dictionary<string, int> gamesWonWithEachCaster = new Dictionary<string, int>();
 
+                CollectBattleReportsData(user, gamesPlayedWithEachFaction, gamesPlayedWithEachTheme, gamesPlayedWithEachCaster, 
+                                         gamesWonWithEachFaction, gamesWonWithEachTheme, gamesWonWithEachCaster);
+
+                string mostPlayedFaction = FindTopResult(gamesPlayedWithEachFaction, out int gamesWithMostPlayedFaction);
+                string mostPlayedTheme = FindTopResult(gamesPlayedWithEachTheme, out int gamesWithMostPlayedTheme);
+                string mostPlayedCaster = FindTopResult(gamesPlayedWithEachCaster, out int gamesWithMostPlayedCaster);
+
                 Dictionary<string, float> winrateWithEachFaction = new Dictionary<string, float>();
                 Dictionary<string, float> winrateWithEachTheme = new Dictionary<string, float>();
                 Dictionary<string, float> winrateWithEachCaster = new Dictionary<string, float>();
 
-                List<BattleReport> usersBattleReports = DatabaseServices.GetBattleReports().Where(br => br.PostersUsername == user.Username || br.OpponentsUsername == user.Username).ToList();
+                FindWinRate(gamesPlayedWithEachFaction, gamesWonWithEachFaction, winrateWithEachFaction);
+                FindWinRate(gamesPlayedWithEachTheme, gamesWonWithEachTheme, winrateWithEachTheme);
+                FindWinRate(gamesPlayedWithEachCaster, gamesWonWithEachCaster, winrateWithEachCaster);
 
-                foreach (BattleReport battleReport in usersBattleReports)
-                {
-                    string faction = user.Username == battleReport.PostersUsername ? battleReport.PostersFaction : battleReport.OpponentsFaction;
-                    if (gamesPlayedWithEachFaction.ContainsKey(faction))
-                        gamesPlayedWithEachFaction[faction]++;
-                    else
-                        gamesPlayedWithEachFaction.Add(faction, 1);
-
-                    if (faction == battleReport.WinningFaction)
-                    {
-                        if (gamesWonWithEachFaction.ContainsKey(faction))
-                            gamesWonWithEachFaction[faction]++;
-                        else
-                            gamesWonWithEachFaction.Add(faction, 1);
-                    }
-
-                    string theme = user.Username == battleReport.PostersUsername ? battleReport.PostersTheme : battleReport.OpponentsTheme;
-                    if (gamesPlayedWithEachTheme.ContainsKey(theme))
-                        gamesPlayedWithEachTheme[theme]++;
-                    else
-                        gamesPlayedWithEachTheme.Add(theme, 1);
-
-                    if (theme == battleReport.WinningTheme)
-                    {
-                        if (gamesWonWithEachTheme.ContainsKey(theme))
-                            gamesWonWithEachTheme[theme]++;
-                        else
-                            gamesWonWithEachTheme.Add(theme, 1);
-                    }
-
-                    string caster = user.Username == battleReport.PostersUsername ? battleReport.PostersCaster : battleReport.OpponentsCaster;
-                    if (gamesPlayedWithEachCaster.ContainsKey(caster))
-                        gamesPlayedWithEachCaster[caster]++;
-                    else
-                        gamesPlayedWithEachCaster.Add(caster, 1);
-
-                    if (caster == battleReport.WinningCaster)
-                    {
-                        if (gamesWonWithEachCaster.ContainsKey(caster))
-                            gamesWonWithEachCaster[caster]++;
-                        else
-                            gamesWonWithEachCaster.Add(caster, 1);
-                    }
-                }
-
-                foreach (var keyValuePair in gamesPlayedWithEachFaction)
-                {
-                    string faction = keyValuePair.Key;
-                    float gamesPlayed = keyValuePair.Value;
-                    float gamesWon = gamesWonWithEachFaction.ContainsKey(faction) ? gamesWonWithEachFaction[faction] : 0;
-                    winrateWithEachFaction.Add(faction, gamesWon / gamesPlayed);
-                }
-
-                foreach (var keyValuePair in gamesPlayedWithEachTheme)
-                {
-                    string theme = keyValuePair.Key;
-                    float gamesPlayed = keyValuePair.Value;
-                    float gamesWon = gamesWonWithEachTheme.ContainsKey(theme) ? gamesWonWithEachTheme[theme] : 0;
-                    winrateWithEachTheme.Add(theme, gamesWon / gamesPlayed);
-                }
-
-                foreach (var keyValuePair in gamesPlayedWithEachCaster)
-                {
-                    string caster = keyValuePair.Key;
-                    float gamesPlayed = keyValuePair.Value;
-                    float gamesWon = gamesWonWithEachCaster.ContainsKey(caster) ? gamesWonWithEachCaster[caster] : 0;
-                    winrateWithEachCaster.Add(caster, gamesWon / gamesPlayed);
-                }
-
-                string mostPlayedFaction = gamesPlayedWithEachFaction.First(f => f.Value == gamesPlayedWithEachFaction.Values.Max()).Key;
-                int gamesWithMostPlayedFaction = gamesPlayedWithEachFaction.Values.Max();
-
-                string mostPlayedTheme = gamesPlayedWithEachTheme.First(t => t.Value == gamesPlayedWithEachTheme.Values.Max()).Key;
-                int gamesWithMostPlayedTheme = gamesPlayedWithEachTheme.Values.Max();
-
-                string mostPlayedCaster = gamesPlayedWithEachCaster.First(c => c.Value == gamesPlayedWithEachCaster.Values.Max()).Key;
-                int gamesWithMostPlayedCaster = gamesPlayedWithEachCaster.Values.Max();
-
-                string bestPerformingFaction = winrateWithEachFaction.First(f => f.Value == winrateWithEachFaction.Values.Max()).Key;
-                float winrateBestPerformingFaction = winrateWithEachFaction.Values.Max();
-
-                string bestPerformingTheme = winrateWithEachTheme.First(t => t.Value == winrateWithEachTheme.Values.Max()).Key;
-                float winrateBestPerformingTheme = winrateWithEachTheme.Values.Max();
-
-                string bestPerformingCaster = winrateWithEachCaster.First(c => c.Value == winrateWithEachCaster.Values.Max()).Key;
-                float winrateBestPerformingCaster = winrateWithEachCaster.Values.Max();
+                string bestPerformingFaction = FindTopResult(winrateWithEachFaction, out float winrateBestPerformingFaction);
+                string bestPerformingTheme = FindTopResult(winrateWithEachTheme, out float winrateBestPerformingTheme);
+                string bestPerformingCaster = FindTopResult(winrateWithEachCaster, out float winrateBestPerformingCaster);
 
                 UserResult usersResult = new UserResult()
                 {
@@ -169,6 +94,86 @@ namespace WMHBattleReporter.ViewModel.Commands
 
                 ViewModel.TopUsersResult.Add(usersResult);
             }
+        }
+
+        private static void CollectBattleReportsData(User user, Dictionary<string, int> gamesPlayedWithEachFaction, Dictionary<string, int> gamesPlayedWithEachTheme, Dictionary<string, int> gamesPlayedWithEachCaster, 
+                                                     Dictionary<string, int> gamesWonWithEachFaction, Dictionary<string, int> gamesWonWithEachTheme, Dictionary<string, int> gamesWonWithEachCaster)
+        {
+            List<BattleReport> usersBattleReports = DatabaseServices.GetBattleReports().Where(br => br.PostersUsername == user.Username || br.OpponentsUsername == user.Username).ToList();
+            foreach (BattleReport battleReport in usersBattleReports)
+            {
+                CollectFactionResults(user, gamesPlayedWithEachFaction, gamesWonWithEachFaction, battleReport);
+                CollectThemeResults(user, gamesPlayedWithEachTheme, gamesWonWithEachTheme, battleReport);
+                CollectCasterResults(user, gamesPlayedWithEachCaster, gamesWonWithEachCaster, battleReport);
+            }
+        }
+
+        private static void CollectFactionResults(User user, Dictionary<string, int> gamesPlayedWithEachFaction, Dictionary<string, int> gamesWonWithEachFaction, BattleReport battleReport)
+        {
+            string faction = user.Username == battleReport.PostersUsername ? battleReport.PostersFaction : battleReport.OpponentsFaction;
+            if (gamesPlayedWithEachFaction.ContainsKey(faction))
+                gamesPlayedWithEachFaction[faction]++;
+            else
+                gamesPlayedWithEachFaction.Add(faction, 1);
+
+            if (faction == battleReport.WinningFaction)
+            {
+                if (gamesWonWithEachFaction.ContainsKey(faction))
+                    gamesWonWithEachFaction[faction]++;
+                else
+                    gamesWonWithEachFaction.Add(faction, 1);
+            }
+        }
+
+        private static void CollectThemeResults(User user, Dictionary<string, int> gamesPlayedWithEachTheme, Dictionary<string, int> gamesWonWithEachTheme, BattleReport battleReport)
+        {
+            string theme = user.Username == battleReport.PostersUsername ? battleReport.PostersTheme : battleReport.OpponentsTheme;
+            if (gamesPlayedWithEachTheme.ContainsKey(theme))
+                gamesPlayedWithEachTheme[theme]++;
+            else
+                gamesPlayedWithEachTheme.Add(theme, 1);
+
+            if (theme == battleReport.WinningTheme)
+            {
+                if (gamesWonWithEachTheme.ContainsKey(theme))
+                    gamesWonWithEachTheme[theme]++;
+                else
+                    gamesWonWithEachTheme.Add(theme, 1);
+            }
+        }
+
+        private static void CollectCasterResults(User user, Dictionary<string, int> gamesPlayedWithEachCaster, Dictionary<string, int> gamesWonWithEachCaster, BattleReport battleReport)
+        {
+            string caster = user.Username == battleReport.PostersUsername ? battleReport.PostersCaster : battleReport.OpponentsCaster;
+            if (gamesPlayedWithEachCaster.ContainsKey(caster))
+                gamesPlayedWithEachCaster[caster]++;
+            else
+                gamesPlayedWithEachCaster.Add(caster, 1);
+
+            if (caster == battleReport.WinningCaster)
+            {
+                if (gamesWonWithEachCaster.ContainsKey(caster))
+                    gamesWonWithEachCaster[caster]++;
+                else
+                    gamesWonWithEachCaster.Add(caster, 1);
+            }
+        }
+
+        private static void FindWinRate(Dictionary<string, int> gamesPlayedWithEachItem, Dictionary<string, int> gamesWonWithEachItem, Dictionary<string, float> winrateWithEachItem)
+        {
+            foreach (var keyValuePair in gamesPlayedWithEachItem)
+            {
+                string item = keyValuePair.Key;
+                float gamesPlayed = keyValuePair.Value;
+                float gamesWon = gamesWonWithEachItem.ContainsKey(item) ? gamesWonWithEachItem[item] : 0;
+                winrateWithEachItem.Add(item, gamesWon / gamesPlayed);
+            }
+        }
+
+        private string FindTopResult<T>(Dictionary<string, T> results, out T value)
+        {
+            value = results.Values.Max();
+            return results.First(kvp => kvp.Value.Equals(results.Values.Max())).Key;
         }
     }
 }
